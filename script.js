71e3a2b67cf57f60ff12ad3e0a81535334f8caba
Kristina Surkova <surkovakristinka@gmail.com>
2023-12-04 20:55:24 +0300

    brands swiper

--- script.js
+++ script.js
@@ -8,29 +8,8 @@ const swiper = new Swiper('.swiper', {
         clickable: true,
     },
 
-    breakpoints: {
-        350: {
-            slidesPerView: 1.25,
-        },
-        480: {
-            slidesPerView: 1.75,
-        },
-        580: {
-            slidesPerView: 2.25,
-        },
-        680: {
-            slidesPerView: 2.5,
-        }
-    },
-
-    touchRatio: 1,
-
-    grabCursor: true,
-
-    centeredSlides: true,
-
-    freeMode: true,
-
-    initialSlide: 0,
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+    cssMode: true,
 
 });
