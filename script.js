0b70caf948e69efaa47ed4884b6dc7969daddf49
Kristina Surkova <surkovakristinka@gmail.com>
2023-12-05 19:50:56 +0300

    brands swiper

--- script.js
+++ script.js
@@ -8,8 +8,57 @@ const swiper = new Swiper('.swiper', {
         clickable: true,
     },
 
-    spaceBetween: 16,
+    breakpoints: {
+        0: {spaceBetween: 16},
+        600: {spaceBetween: 0},
+    },
+
     slidesPerView: 'auto',
     cssMode: true,
 
 });
+
+let element = document.querySelector('.swiper-slide')
+
+if (window.innerWidth > 600) {
+    let wrapSize = document.querySelector('.brandsSwiper');
+    let wrap = document.querySelector('.swiper-wrapper');
+    wrap.classList.add("grid");
+    wrap.classList.add("grid--hidden");
+    wrap.classList.remove('swiper-wrapper');
+    wrapSize.classList.add('swiper--size');
+    wrapSize.classList.remove('swiper');
+    element.style.marginRight = '32px';
+
+    console.log(wrap);
+    console.log(wrapSize);
+
+    let buttonM = wrapSize.querySelector('.buttonMore');
+    let buttonL = wrapSize.querySelector('.buttonLess');
+
+    if (buttonM) {
+        buttonM.addEventListener('click', function() {
+            wrap.classList.add('grid--visible');
+            wrap.classList.remove('grid--hidden');
+            wrapSize.style.height = 'auto';
+            buttonM.textContent = 'Скрыть';
+            buttonM.classList.add('buttonLess');
+            buttonM.classList.remove('buttonMore');
+        });
+    } else if (buttonL) {
+        buttonL.addEventListener('click', function() {
+            wrap.classList.add('grid--hidden');
+            wrap.classList.remove('grid--visible');
+            buttonL.textContent = 'Показать все';
+            buttonL.classList.add('buttonMore');
+            buttonL.classList.remove('buttonLess');
+        });
+    }
+
+
+}
+
+
+
+
+
