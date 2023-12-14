62d25d3f0721fe7f31a7fdac9737a89e8801033c
Kristina Surkova <surkovakristinka@gmail.com>
2023-12-14 23:11:12 +0300

    error correction v3

--- script.js
+++ script.js
@@ -44,7 +44,20 @@ if (window.innerWidth >= 768) {
             buttonSwiper.classList.add('buttonMore');
         }
     });
+/*
+    let showMore = function (button, spoiler, showMoreText) {
+        button.addEventListener('click', function () {
+            spoiler.classList.toggle('active');
+            button.classList.toggle('active');
+            if (button.innerText === showMoreText) button.innerText = 'Скрыть';
+            else button.innerText = showMoreText;
+        })
+    };
+
+ */
+
 }
+
 
 /*
 let element = document.querySelector('.swiper-slide')
